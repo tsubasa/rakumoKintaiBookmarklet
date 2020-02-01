bf4427c9c673af388bde8adc3d6d302034db844a
tsubasa <m@tsubasa.me>
2020-02-02 00:00:01 +0900

    add testEnvironment in jest config

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
 };
