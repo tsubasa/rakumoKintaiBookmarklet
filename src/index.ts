ba2c2975dc473983fd0e50fa09778567d8598b4e
tsubasa <m@tsubasa.me>
2020-02-02 00:04:31 +0900

    RakumoKintaiクラスを削除

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable max-classes-per-file, class-methods-use-this */
+import { RakumoKintaiApp, RakumoKintaiCalculator } from './classes';
 
 // types
 type RakumoKintaiAppConfig = {
@@ -12,40 +12,11 @@ type RakumoKintaiAppConfig = {
   };
 };
 
-interface Window {
+interface MyWindow extends Window {
   appConfig: RakumoKintaiAppConfig;
 }
 
-type LeaveUnit = 'full-day' | 'half-day';
-
-type LeaveUnitType = 'am' | 'pm';
-
-type RecordFlow = { params: { leaveUnitType: LeaveUnitType; leaveUnit: LeaveUnit } };
-
-type AttendanceReport = {
-  periodId: number;
-};
-
-type AttendanceRecord = {
-  date: string; // 日付
-  actualWorkingMinutes: number; // 実働時間
-  checkInLateMinutes: number; // 遅刻時間
-  workingDay?: {
-    workingMinutes: number; // 1日に必要な労働時間
-    breaks: { startTime: string; endTime: string }[]; // 1日の休憩時間
-  };
-  flows: RecordFlow[];
-  checkInStamp: { roundedDatetime: string } | null; // 出勤が打刻されると反映
-  checkOutStamp: {} | null; // 退勤が打刻されると反映
-};
-
-type AttendanceReports = { items: AttendanceReport[] };
-
-type AttendanceRecords = { items: AttendanceRecord[] };
-
-type AttendanceReportsRequestParams = { periodId: string; userKey: string };
-
-type AttendanceRecordsRequestParams = { periodId: string };
+declare const window: MyWindow;
 
 // utilities
 const getPeriodId = (): number | undefined => {
@@ -72,131 +43,6 @@ const formattedTime = (minutes: number): string => {
   if (isMinus) minutes *= -1; // eslint-disable-line no-param-reassign
   return `${isMinus ? '-' : ''}${Math.floor(minutes / 60)}:${String(minutes % 60).padStart(2, '0')}`;
 };
-
-// classes
-class RakumoKintaiApp {
-  private readonly userId: string;
-
-  private readonly defaultPeriodId: number;
-
-  private readonly BASE_API_URL = 'https://a-rakumo.appspot.com/api/attendance/v1';
-
-  public constructor(userId: string, defaultPeriodId: number) {
-    this.userId = userId;
-    this.defaultPeriodId = defaultPeriodId;
-  }
-
-  public async getReports(): Promise<AttendanceReports> {
-    return this.req<AttendanceReports, AttendanceReportsRequestParams>('reports', {
-      periodId: `${this.defaultPeriodId}`,
-      userKey: this.userId
-    });
-  }
-
-  public async getLatestReport(): Promise<AttendanceReport | undefined> {
-    const reports = await this.getReports();
-    return reports.items.shift();
-  }
-
-  public async getRecords(periodId: number): Promise<AttendanceRecords> {
-    return this.req<AttendanceRecords, AttendanceRecordsRequestParams>(`records/${this.userId}`, {
-      periodId: `${periodId}`
-    });
-  }
-
-  private async req<T, P extends { [key: string]: string }>(path: string, params?: P): Promise<T> {
-    return fetch(`${this.BASE_API_URL}/${path}?${new URLSearchParams(params)}`).then(res => res.json());
-  }
-}
-
-class RakumoKintaiCalculator {
-  private workingMinutes = 0;
-
-  private actualWorkingMinutes = 0;
-
-  private notWorkingMinutes = 0;
-
-  private todayCheckInLateMinutes = 0;
-
-  private readonly AM_OFF = 210;
-
-  private readonly PM_OFF = 270;
-
-  public constructor(records: AttendanceRecord[]) {
-    records.forEach(v => this.calc(v));
-  }
-
-  private calc(record: AttendanceRecord): void {
-    // 出勤日
-    if (record.workingDay) {
-      // 過去から当日までのデータを集計
-      if (!this.isAfterDate(record.date)) {
-        // 出退勤が打刻済み
-        if (record.checkInStamp && record.checkOutStamp) {
-          this.workingMinutes += record.workingDay.workingMinutes;
-          this.actualWorkingMinutes += record.actualWorkingMinutes;
-          this.notWorkingMinutes += this.calcNotWorkingMinutes(record.flows);
-        }
-        // 出勤のみが打刻済み(当日のデータ)
-        else if (this.isEqualDate(record.date) && record.checkInStamp && !record.checkOutStamp) {
-          const breakMinutes = record.workingDay.breaks.reduce(
-            (acc, cur) => acc + this.calcDiffMinutes(cur.endTime, cur.startTime),
-            0
-          );
-          const actualWorkingMinutes =
-            this.calcDiffMinutes(new Date().toISOString(), record.checkInStamp.roundedDatetime) - breakMinutes;
-
-          this.workingMinutes += record.workingDay.workingMinutes;
-          this.actualWorkingMinutes += actualWorkingMinutes < 0 ? 0 : actualWorkingMinutes;
-          this.notWorkingMinutes += this.calcNotWorkingMinutes(record.flows);
-
-          // 当日のペナルティを保持する(日別集計前のため)
-          this.todayCheckInLateMinutes += record.checkInLateMinutes;
-        }
-      }
-    }
-  }
-
-  private calcNotWorkingMinutes(flows: RecordFlow[]): number {
-    return flows.reduce((acc, cur) => {
-      const { leaveUnit, leaveUnitType } = cur.params;
-
-      if (leaveUnit === 'full-day') {
-        return acc + this.AM_OFF + this.PM_OFF;
-      }
-
-      if (leaveUnit === 'half-day') {
-        if (leaveUnitType === 'am') return acc + this.AM_OFF;
-        if (leaveUnitType === 'pm') return acc + this.PM_OFF;
-      }
-
-      return acc;
-    }, 0);
-  }
-
-  private calcDiffMinutes(date1: string, date2: string): number {
-    const diffTime = new Date(date1).getTime() - new Date(date2).getTime();
-    return diffTime !== 0 ? Math.round(diffTime / 1000 / 60) : 0;
-  }
-
-  private isAfterDate(date: string): boolean {
-    return new Date(this.getTodayDate()).getTime() < new Date(date).getTime();
-  }
-
-  private isEqualDate(date: string): boolean {
-    return new Date(this.getTodayDate()).getTime() === new Date(date).getTime();
-  }
-
-  private getTodayDate(): string {
-    return new Date().toISOString().split('T')[0];
-  }
-
-  public get overtimeWorkingMinutes(): number {
-    const overtimeWorkingMinutes =
-      this.actualWorkingMinutes - this.workingMinutes + this.todayCheckInLateMinutes + this.notWorkingMinutes;
-    return overtimeWorkingMinutes < 0 ? 0 : overtimeWorkingMinutes;
-  }
-}
 
 // main
 (async (): Promise<void> => {
