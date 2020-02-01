4457cb047a7bd1d622d4b7972c231585fac1b6fb
tsubasa <m@tsubasa.me>
2020-02-02 00:01:41 +0900

    ビルド環境修正

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const terser = require('gulp-terser');
 
-const tmpJsPath = './build/index.js';
+const tmpTsPath = './src/tmp.ts';
+const tmpJsPath = './build/tmp.js';
 
 gulp.task('build', async () => {
   return new Promise(resolve => {
@@ -31,14 +32,38 @@ gulp.task('build', async () => {
       .pipe(rename('bookmarklet.js'))
       .pipe(gulp.dest('build'))
       .on('end', () => {
-        fs.unlink(tmpJsPath, err => {
-          if (err && err.code === 'ENOENT') {
-            console.info("File doesn't exist, won't remove it.");
-          } else if (err) {
-            console.error('Error occurred while trying to remove file');
-          }
-        });
+        [tmpTsPath, 'build/classes.js', 'build/index.js', 'build/tmp.js'].forEach(file =>
+          fs.unlink(file, err => {
+            if (err) throw err;
+          })
+        );
         resolve();
       });
   });
+});
+
+gulp.task('setup', async () => {
+  const readFile = filePath => {
+    return new Promise(resolve => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw err;
+        resolve(data);
+      });
+    });
+  };
+
+  const writeFile = (filePath, data) => {
+    return new Promise(resolve => {
+      fs.writeFile(filePath, data, err => {
+        if (err) throw err;
+        resolve();
+      });
+    });
+  };
+
+  let data = await readFile('./src/classes.ts');
+  data += await readFile('./src/index.ts');
+  data = data.replace(/^(import|export|declare)\s/gm, '// $1 ');
+  data = data.replace(/^interface MyWindow extends /gm, 'interface ');
+  await writeFile(tmpTsPath, data);
 });
